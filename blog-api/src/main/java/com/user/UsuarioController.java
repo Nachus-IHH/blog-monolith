e5242d80a7bb9450e9fb5d2b319c0dfe9981e119
Nachus-IHH <2012.hernandez.hernandez@gmail.com>
2026-06-16 11:36:19 -0600

    refa: integration testing step on creating user v1.0

--- blog-api/src/main/java/com/user/UsuarioController.java
+++ blog-api/src/main/java/com/user/UsuarioController.java
@@ -1,7 +1,9 @@
 package com.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.config.ApiPaths;
 import com.user.dto.RequestUsuarioDto;
@@ -37,8 +41,18 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<ResponseUsuarioDto> crearUsuario(
             @RequestBody RequestUsuarioDto request) {
-        return ResponseEntity.ok(
-                usuarioService.crearUsuario(request));
+
+        ResponseUsuarioDto nuevoUsuario = usuarioService.crearUsuario(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(nuevoUsuario.id())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(nuevoUsuario);
     }
 
     /**
